9ad86b9c767a045608b666d17207d454de37e433
Quang <hongquang7302@gmail.com>
2023-03-17 09:05:34 +0700

    first commit

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
+  getUserEdit
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/edit", verifyToken, getUserEdit);
+router.get("/:id/friends", verifyToken, getUserFriends);
+
+/* UPDATE */
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+
+export default router;
